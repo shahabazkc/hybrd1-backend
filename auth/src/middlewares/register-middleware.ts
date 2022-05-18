8981877a1e2f389d6678e02ff1084cee2244aeae
Shahabaz Kc <55649768+shahabazkc@users.noreply.github.com>
2022-05-19 00:29:44 +0530

    auth: registeration router created

---
+++ auth/src/middlewares/register-middleware.ts
@@ -0,0 +1,17 @@
+import { body } from "express-validator";
+
+export const registerMiddleware = [
+    body('username')
+        .isString()
+        .trim()
+        .isLength({ min: 2, max: 20 })
+        .withMessage('Username must be in between 2 and 20 characters'),
+    body('password')
+        .isString()
+        .isLength({ min: 4, max: 22 })
+        .withMessage('Password must be in between 2 and 22 characters'),
+    body('typeOfUser')
+        .isString()
+        .custom((value)=> value === 'buyer'||'seller')
+        .withMessage('Type of user must be valid')
+];
---
+++ auth/src/routes/register-user.ts
@@ -0,0 +1,41 @@
+import { BadRequestError, validateRequest } from '@hybrd1/common';
+import express, { Request, Response } from 'express';
+import { registerMiddleware } from '../middlewares/register-middleware';
+import { User } from '../models/user';
+
+const router = express.Router();
+
+router.post("/",
+    registerMiddleware, validateRequest, // Body Middlewares 
+    async (req: Request, res: Response) => {
+
+        let {
+            username,
+            password,
+            typeOfUser
+        } = req.body;
+
+
+        // Finding the user is already in db with the same username
+        const user = await User.findOne({ username });
+
+        if (user) throw new BadRequestError('User already found');
+
+        // Hash the password here
+
+        const userBuild = User.build({
+            username,
+            password,
+            type_of_user: typeOfUser
+        });
+
+        // Save to Db
+        await userBuild.save();
+
+        res.json({ status: true, data: userBuild })
+
+    }
+);
+
+
+export { router as registerBuyerRouter };
