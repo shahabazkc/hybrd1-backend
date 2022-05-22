b06ca11a1f0449e7686f46914a2ab6191e35548a
Shahabaz Kc <55649768+shahabazkc@users.noreply.github.com>
2022-05-22 19:12:35 +0530

    Removed: Unwanted middleware condition removed

--- common-service/src/middlewares/create-catalogue-middleware.ts
+++ common-service/src/middlewares/create-catalogue-middleware.ts
@@ -4,8 +4,5 @@ export const createCatalogueMiddleware = [
     body('name')
         .isString()
         .isLength({ min: 2, max: 15 })
-        .withMessage('Name must be in between 2 and 15 characters'),
-    body('products')
-        .isArray({ min: 1 })
-        .withMessage('products must contain 1 or more items')
+        .withMessage('Name must be in between 2 and 15 characters')
 ]
